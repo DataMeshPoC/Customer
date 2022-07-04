b0854f0773be20994c72decace9e9413e67a9130
talyawkli <90151339+talyawkli@users.noreply.github.com>
2022-07-04 12:21:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from threading import Event
-# import signal
-import pyodbc
+import signal
+import pypyodbc as pyodbc
 import pysftp
 import pandas as pd
 
@@ -22,7 +22,7 @@ cxnx = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database
 cursor = cxnx.cursor()
 
 # Configure application
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -35,36 +35,7 @@ Session(app)
 
 INTERRUPT_EVENT = Event()
 
-# def get_kafka_producer():
-#     return Producer({'sasl.mechanisms':'PLAIN',
-#                      'request.timeout.ms': 20000,
-#                      'bootstrap.servers':'pkc-epwny.eastus.azure.confluent.cloud:9092',
-#                      'retry.backoff.ms':500,
-#                      'sasl.username':'IHO7XVPCJCCBZAYX',
-#                      'sasl.password':'UAwjmSIn5xuAL7HZmBjU4NGt0nLfXbyjtlVA7imgCdGBYFkog5kw0gc4e5MYmiUE',
-#                      'security.protocol':'SASL_SSL'})
-
 logging.basicConfig(level=logging.DEBUG)
-
-# Create a Kafka Producer
-# producer=get_kafka_producer()
-
-# client = FlaskKafka(INTERRUPT_EVENT,
-#                  bootstrap_servers=",".join(["localhost:9092"]),
-#                  group_id="consumer-grp-id"
-#                  )
-
-# # update this
-# @client.handle('customers')
-# def test_topic_handler(msg):
-#     print("consumed {} from test-topic".format(msg))
-
-# def listen_kill_server():
-#     signal.signal(signal.SIGTERM, client.interrupted_process)
-#     signal.signal(signal.SIGINT, client.interrupted_process)
-#     signal.signal(signal.SIGQUIT, client.interrupted_process)
-#     signal.signal(signal.SIGHUP, client.interrupted_process)
-# run the app.
 
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
@@ -96,32 +67,28 @@ def buy():
     """Buy policies"""
     if request.method == "POST":
 
-        Name = request.form.get("Name")
-        startdate = request.form.get("startdate")
-        dob = request.form.get("dob")
-        email = request.form.get("email")
-        gender = request.form.get("gender")
-        premium = request.form.get("premium")
-        policytype = request.form.get("policytype")
+        # Input desired plan
+        name = request.form.get("Name")
+        type = request.form.get("Type")
+        category = request.form.get("Category")
 
         # If no symbols are inputted
-        if not Name or not startdate or not dob or not email or not gender or not premium or not policytype:
+        if not name or not type or not category:
             return apology("Missing field")
 
-      #  Query the users database for the users, product database for the policy
-        users = cursor.execute("SELECT * FROM fwd_poc.products WHERE name=Name")
-        policyP = cursor.execute("SELECT * FROM fwd_poc.products WHERE id = :id", id=id)
+      #  Query the product database for the policies that can be bought
+        policyP = cursor.execute("SELECT * FROM fwd_poc.products WHERE type = :type", type=type)
        # Error check for search result
-        if not users:
-            apology("Invalid user information")
         if not policyP:
-            apology("Invalid policy information")
+            apology("Policy does not exist")
 
         typ = policyP[0]["Type"]
-        inputtype = users[0]["policytype"]
 
        # Update stream with the new buyer information
-        results = cursor.api_client.inserts_stream("my_stream_name", users=users, use_http2=True)
+        results = cursor.api_client.inserts_stream("my_stream_name", typ=typ, use_http2=True)
+
+        # FIX ABOVE
+
         flash("Bought!")
         return redirect("/")
 
@@ -137,21 +104,14 @@ def login():
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
+        email = request.form.get("email")
 
         # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
-
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 403)
+        if not email:
+            return apology("must provide email", 403)
 
         # Query database for username
-        rows = cursor.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"), use_http2=True)
-
-        # Ensure username exists and password is correct
-        if len(rows) != 1:
-            return apology("invalid username and/or password", 403)
+        rows = cursor.execute("SELECT * FROM users WHERE username = email", request.form.get("email"), use_http2=True)
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
@@ -178,25 +138,17 @@ def logout():
 def register():
     # Make sure that the users reached routes via POST
     if request.method == "POST":
-        new_username = str(request.form.get("username"))
-        password = str(request.form.get("password"))
-        confirmation = str(request.form.get("confirmation"))
+        email = str(request.form.get("email"))
+        name = str(request.form.get("name"))
 
         # Validate submission; ensure username was submitted
-        if not new_username:
-            return apology("Invalid username")
+        if not email:
+            return apology("Input email please.")
 
-        if not password or not confirmation:
-            return apology("Invalid password")
-
-        if password != confirmation:
-            return apology("Passwords don't match")
-
-         # Add user to database
+         # Check if login information is already taken
         try:
-            id = cursor.execute("INSERT INTO users (username, hash) VALUES(?, ?)",
-                            request.form.get("username"),
-                            (request.form.get("password")))
+            id = cursor.execute("INSERT INTO dbo.customers (email, name) VALUES(?, ?)",
+                            request.form.get("email"), request.form.get("email"))
         except ValueError:
             return apology("username taken")
 
@@ -207,7 +159,7 @@ def register():
         flash("Registered!")
 
         # Insert the new login information from register into the users table
-        rows = cursor.api_client.inserts_stream("my_stream_name", password, new_username, session["user_id"])
+        rows = cursor.api_client.inserts_stream("my_stream_name", email, name, session["user_id"])
         session["user_id"] = rows
 
     # Confirm registration
