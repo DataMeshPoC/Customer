5a7c530562312c112d1e7cb488195246b1a0c340
Mikanribu <clementine.pages@gmail.com>
2022-07-08 00:02:33 +0800

    update policy table name

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def index():
     
 #     Posting to the database for buying
     if request.method == "POST":
-        sql= f"INSERT INTO dbo.policy (name, term, type, customeremail, premiumpayment,premiumstructure, description, currency, policystatus) VALUES " \
+        sql= f"INSERT INTO dbo.policydraft (name, term, type, customeremail, premiumpayment,premiumstructure, description, currency, policystatus) VALUES " \
              f"('{request.form.get('name')}', '{request.form.get('term')}', '{request.form.get('type')}', '{session.get('info')[4]}', " \
              f"'{request.form.get('premiumpayment')}', '{request.form.get('premiumstructure')}', '{request.form.get('desc')}', 'HKD', 'Draft')"
         print(sql)
